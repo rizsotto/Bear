b35c42e4393158d025389affb5e10958de7425e3
Laszlo Nagy <rizsotto@gmail.com>
2012-12-08 11:43:17 +0100

    use string array method in report logic

--- src/lib/report.c
+++ src/lib/report.c
@@ -1,6 +1,7 @@
 // This file is distributed under MIT-LICENSE. See COPYING for details.
 
 #include "report.h"
+#include "../common/stringarray.h"
 
 #include <unistd.h>
 #include <malloc.h>
@@ -13,7 +14,7 @@
 #include <sys/un.h>
 
 static void write_cwd(int fd);
-static void write_call(int fd, char const * const argv[]);
+static void write_call(int fd, char const * argv[]);
 
 void report_call(char const *method, char * const argv[]) {
     // get output file name
@@ -32,12 +33,11 @@ void report_call(char const *method, char * const argv[]) {
             return;
         }
         write_cwd(s);
-        write_call(s, (char const * const *)argv);
+        write_call(s, (char const **)argv);
         close(s);
     }
 }
 
-static char const * concatenate(char const * const argv[]);
 static void write_string(int fd, char const * message);
 
 static void write_cwd(int fd) {
@@ -46,8 +46,8 @@ static void write_cwd(int fd) {
     free((void*)cwd);
 }
 
-static void write_call(int fd, char const * const argv[]) {
-    char const * cmd = concatenate(argv);
+static void write_call(int fd, char const * argv[]) {
+    char const * cmd = sa_fold(argv);
     write_string(fd, cmd);
     free((void *)cmd);
 }
@@ -58,25 +58,5 @@ static void write_string(int fd, char const * message) {
     if (length > 0) {
         write(fd, (void const *)message, length);
     }
-}
-
-static char const * concatenate(char const * const argv[]) {
-    char * acc = 0;
-    size_t acc_size = 0;
-
-    char const * const * it = argv;
-    for (;*it;++it) {
-        size_t const sep = (argv == it) ? 0 : 1;
-        size_t const it_size = strlen(*it);
-        acc = (char *)realloc(acc, acc_size + sep + it_size);
-        if (sep) {
-            acc[acc_size++] = ' ';
-        }
-        strncpy((acc + acc_size), *it, it_size);
-        acc_size += it_size;
-    }
-    acc = (char *)realloc(acc, acc_size + 1);
-    acc[acc_size++] = '\0';
-    return acc;
 }
 
