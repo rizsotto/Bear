33be6f7499b037a8e493959a56964362e7fbc543
Ben Reser <ben@reser.org>
2013-04-23 21:40:18 -0700

    On the Mac use DYLD_INSERT_LIBRARIES instead of LD_PRELOAD

--- src/environ.h
+++ src/environ.h
@@ -3,7 +3,11 @@
 #ifndef BEAR_ENVIRON_H
 #define BEAR_ENVIRON_H
 
+#ifdef __APPLE__
+#define ENV_PRELOAD "DYLD_INSERT_LIBRARIES"
+#else
 #define ENV_PRELOAD "LD_PRELOAD"
+#endif
 #define ENV_OUTPUT  "BEAR_OUTPUT"
 
 char const * * bear_env_insert(char const * * in, char const * key, char const * value);
