22c6e2b96114b8f637f5cb102d0f7b832a6ac8f9
Laszlo Nagy <rizsotto@gmail.com>
2013-06-09 12:35:11 +0200

    add new end to end test based on shell

---
+++ test/end-to-end/compile.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+set -o xtrace
+
+g++ -c -fPIC -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
+g++ -c -fPIC -I"$1/lib" "$1/lib/bar.cpp" -o /dev/null
+
+# add noise to the compilation...
+true
+echo "gcc -invocation -look -like this.c"
+
+(g++ -c -I"$1/lib" -I"$1/bin" "$1/bin/boo.cpp" -o /dev/null)
+(g++ -c -I"$1/lib" -I"$1/bin" "$1/bin/far.cpp" -o /dev/null)
+
+# add noise to the compilation...
+echo "g++ -c bin/boo.cpp -o /dev/null"
