5f41db82a4c6bd3b3183056eebf38a2fda1f68ca
Laszlo Nagy <rizsotto@gmail.com>
2014-05-08 13:12:29 +0200

    call cc/c++ instead of gcc/g++ in tests

--- test/end-to-end/compile.sh
+++ test/end-to-end/compile.sh
@@ -4,20 +4,20 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
-g++ -c -fPIC -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
-g++ -c -fPIC -I"$1/lib" "$1/lib/bar.cc" -o /dev/null
+c++ -c -fPIC -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
+c++ -c -fPIC -I"$1/lib" "$1/lib/bar.cc" -o /dev/null
 
-g++ -M -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
-g++ -MM -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
-g++ -MM -MG -I"$1/lib" "$1/lib/bar.cc" -o /dev/null
-g++ -I"$1/lib" "$1/lib/bar.cc" -M -o /dev/null
+c++ -M -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
+c++ -MM -I"$1/lib" "$1/lib/foo.cpp" -o /dev/null
+c++ -MM -MG -I"$1/lib" "$1/lib/bar.cc" -o /dev/null
+c++ -I"$1/lib" "$1/lib/bar.cc" -M -o /dev/null
 
 # add noise to the compilation...
 true
 echo "gcc -invocation -look -like this.c"
 
-(g++ -c -I"$1/lib" -I"$1/bin" "$1/bin/./boo.c++" -o /dev/null)
-(g++ -c -I"$1/lib" -I"$1/bin" "$1/bin/../bin/far.cxx" -o /dev/null)
+(c++ -c -I"$1/lib" -I"$1/bin" "$1/bin/./boo.c++" -o /dev/null)
+(c++ -c -I"$1/lib" -I"$1/bin" "$1/bin/../bin/far.cxx" -o /dev/null)
 
 # add noise to the compilation...
-echo "g++ -c bin/boo.cpp -o /dev/null"
+echo "c++ -c bin/boo.cpp -o /dev/null"
