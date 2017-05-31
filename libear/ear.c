6b07f5044f30a3070d1dc39801bcdd94395d673e
Laszlo Nagy <rizsotto@gmail.com>
2017-05-31 21:22:27 +1000

    fix 0 length argument problem

--- libear/ear.c
+++ libear/ear.c
@@ -45,6 +45,7 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <errno.h>
 
 #if defined HAVE_POSIX_SPAWN || defined HAVE_POSIX_SPAWNP
 #include <spawn.h>
@@ -473,7 +474,7 @@ static int bear_write_json_report(int fd, char const *const cmd[], char const *c
 
     for (char const *const *it = cmd; (it) && (*it); ++it) {
         char const *const sep = (it != cmd) ? "," : "";
-        const size_t buffer_size = 6 * strlen(*it);
+        const size_t buffer_size = (6 * strlen(*it)) + 1;
         char buffer[buffer_size];
         if (-1 == bear_encode_json_string(*it, buffer, buffer_size))
             return -1;
