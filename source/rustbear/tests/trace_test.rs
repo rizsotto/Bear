38b35930a17a06b94a833e0804811d9c1d72965b
Laszlo Nagy <rizsotto@gmail.com>
2018-10-27 22:37:57 +1100

    integration test review

--- source/rustbear/tests/trace_test.rs
+++ source/rustbear/tests/trace_test.rs
@@ -4,20 +4,23 @@ use std::path;
 use std::fs;
 use intercept::trace::Trace;
 
+const TEST_FILE_NAME: &'static str = "execution.test.read_write_works.json";
+
 #[test]
 fn read_write_works() {
-    let args: Vec<_> = ["a", "b", "c"].iter().map(|s| s.to_string()).collect();
-    let dir = path::Path::new("/tmp").to_path_buf();
-    let value = Trace::new(1234, dir, args);
+    let expected = Trace::new(1234,
+                              path::PathBuf::from("/tmp"),
+                              vec!["a".to_string(), "b".to_string()]);
     {
-        let mut file = fs::File::create("execution.test.read_write_works.json").unwrap();
-        let _result = Trace::write(&mut file, &value);
+        let mut file = fs::File::create(TEST_FILE_NAME).unwrap();
+        let _result = Trace::write(&mut file, &expected);
     }
     {
-        let mut file = fs::File::open("execution.test.read_write_works.json").unwrap();
+        let mut file = fs::File::open(TEST_FILE_NAME).unwrap();
         let result = Trace::read(&mut file).unwrap();
-        assert_eq!(value.get_pid(), result.get_pid());
-        assert_eq!(value.get_cwd(), result.get_cwd());
-        assert_eq!(value.get_cmd(), result.get_cmd());
+        assert_eq!(expected.get_pid(), result.get_pid());
+        assert_eq!(expected.get_cwd(), result.get_cwd());
+        assert_eq!(expected.get_cmd(), result.get_cmd());
     }
+    fs::remove_file(TEST_FILE_NAME).unwrap();
 }
