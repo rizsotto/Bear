3e6f5b821472dadf4c9f547993b5e4ec87de1712
Laszlo Nagy <rizsotto@gmail.com>
2018-11-21 17:08:58 +1100

    add wrapper draft

---
+++ rust/wrappers/src/bin/cxx.rs
@@ -0,0 +1,63 @@
+/*  Copyright (C) 2012-2018 by László Nagy
+    This file is part of Bear.
+
+    Bear is a tool to generate compilation database for clang tooling.
+
+    Bear is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Bear is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#[macro_use]
+extern crate clap;
+extern crate env_logger;
+#[macro_use]
+extern crate error_chain;
+#[macro_use]
+extern crate log;
+extern crate wrapper;
+
+use std::env;
+use std::path;
+use std::process;
+use wrapper::{Error, Result};
+
+fn main() {
+    if let Err(ref e) = run() {
+        eprintln!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            eprintln!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            eprintln!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    const CONFIG_FLAG: &str = "config";
+    const OUTPUT_FLAG: &str = "output";
+    const BUILD_FLAG: &str = "build";
+
+    drop(env_logger::init());
+    info!("{} {}", crate_name!(), crate_version!());
+
+    let args: Vec<String> = env::args().collect();
+    debug!("invocation: {:?}", &args);
+
+    Ok(())
+}
