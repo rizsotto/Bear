2f10e5c5293d601c57702f2c4c6c0deb8163c5dc
Laszlo Nagy <rizsotto@gmail.com>
2019-02-13 12:43:00 +1100

    create spawn to simplify unix supervisor

--- rust/src/supervisor/unix.rs
+++ rust/src/supervisor/unix.rs
@@ -43,54 +43,22 @@ impl<'a> Supervisor<'a> {
     pub fn run(&mut self, cmd: &[String]) -> Result<ExitCode> {
         let cwd = env::current_dir()
             .chain_err(|| "unable to get current working directory")?;
-        let (read_fd, write_fd) = pipe()?;
 
-        match fork() {
-            Ok(ForkResult::Parent { child, .. }) => {
-                debug!("Parent process: waiting for pid: {}", child);
-                close(write_fd);
-
-                let mut buffer = vec![0u8, 10];
-                match read(read_fd, buffer.as_mut()) {
-                    // In case of successful start the child closed the pipe,
-                    // so we can't read anything from it.
-                    Ok(0) => {
-                        let event = Event::Created(
-                            ProcessCreated {
-                                pid: child.as_raw() as ProcessId,
-                                ppid: Pid::parent().as_raw() as ProcessId,
-                                cwd: cwd.clone(),
-                                cmd: cmd.to_vec(),
-                            },
-                            chrono::Utc::now());
-                        self.report(event);
-                        self.wait_for_pid(child)
+        match spawn(cmd) {
+            Ok(pid) => {
+                let event = Event::Created(
+                    ProcessCreated {
+                        pid: pid.as_raw() as ProcessId,
+                        ppid: Pid::parent().as_raw() as ProcessId,
+                        cwd: cwd.clone(),
+                        cmd: cmd.to_vec(),
                     },
-                    // If the child failed to exec the given process, it
-                    // sends us a message through the pipe.
-                    Ok(length) => {
-                        bail!("Could not execute process. {:?}", cmd[0])
-                    },
-                    // Not sure if this will happen, when we can't read.
-                    Err(_) => {
-                        bail!("Read failed.")
-                    },
-                }
-            }
-            Ok(ForkResult::Child) => {
-                debug!("Child process: will call exec soon.");
-                close(read_fd);
-
-                let args = to_c_string(cmd);
-                execvp(&args[0], &args)
-                    .map(|_| /* Not going to execute this. */ 0)
-                    .chain_err(|| {
-                        write(write_fd, b"error");
-                        format!("exec failed: {:?}", cmd)
-                    })
+                    chrono::Utc::now());
+                self.report(event);
+                self.wait_for_pid(pid)
             },
             Err(_) => {
-                bail!("Could not fork process")
+                bail!("Could not execute process")
             },
         }
     }
@@ -154,9 +122,44 @@ impl<'a> Supervisor<'a> {
     }
 }
 
-fn to_c_string(cmd: &[String]) -> Vec<ffi::CString> {
-    let result: Vec<_> = cmd.iter()
-        .map(|arg| ffi::CString::new(arg.as_bytes()).unwrap())
-        .collect();
-    result
+fn spawn(cmd: &[String]) -> Result<Pid> {
+    let (read_fd, write_fd) = pipe()?;
+
+    match fork() {
+        Ok(ForkResult::Parent { child, .. }) => {
+            debug!("Parent process: waiting for pid: {}", child);
+            close(write_fd);
+
+            let mut buffer = vec![0u8, 10];
+            match read(read_fd, buffer.as_mut()) {
+                // In case of successful start the child closed the pipe,
+                // so we can't read anything from it.
+                Ok(0) =>
+                    Ok(child),
+                // If the child failed to exec the given process, it
+                // sends us a message through the pipe.
+                Ok(length) =>
+                    bail!("Could not execute process. {:?}", cmd[0]),
+                // Not sure if this will happen, when we can't read.
+                Err(_) =>
+                    bail!("Read failed."),
+            }
+        }
+        Ok(ForkResult::Child) => {
+            debug!("Child process: will call exec soon.");
+            close(read_fd);
+
+            let args : Vec<_> = cmd.iter()
+                .map(|arg| ffi::CString::new(arg.as_bytes()).unwrap())
+                .collect();
+            execvp(&args[0], args.as_ref())
+                .map(|_| /* Not going to execute this. */ 0)
+                .map_err(|_| {
+                    write(write_fd, b"error");
+                });
+            bail!("exec failed: {:?}", cmd)
+        },
+        Err(_) =>
+            bail!("Could not fork process"),
+    }
 }
