ecde1a05fa0e6a00219e05f0f0bbd820dcf7c183
Laszlo Nagy <rizsotto@gmail.com>
2019-02-21 19:48:36 +1100

    add unit test for generic supervisor

--- rust/src/supervisor.rs
+++ rust/src/supervisor.rs
@@ -89,6 +89,11 @@ pub fn get_parent_pid() -> ProcessId {
     }
 }
 
+#[cfg(test)]
+macro_rules! slice_of_strings {
+    ($($x:expr),*) => (vec![$($x.to_string()),*].as_ref());
+}
+
 #[cfg(unix)]
 mod unix {
     use std::ffi;
@@ -255,10 +260,6 @@ mod unix {
     #[cfg(test)]
     mod test {
         use super::*;
-
-        macro_rules! slice_of_strings {
-            ($($x:expr),*) => (vec![$($x.to_string()),*].as_ref());
-        }
 
         mod test_exit_code {
             use super::*;
@@ -487,6 +488,99 @@ mod generic {
                     warn!("Child process was not running: {:?}", handle.child.id());
                     Err(Error::with_chain(error, "Waiting for process failed."))
                 }
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        mod test_exit_code {
+            use super::*;
+
+            #[test]
+            fn success() {
+                let mut sink = |_: Event| ();
+
+                let result = super::ProcessHandle::run(&mut sink, slice_of_strings!("/usr/bin/true"));
+                assert_eq!(true, result.is_ok());
+                assert_eq!(0i32, result.unwrap());
+            }
+
+            #[test]
+            fn fail() {
+                let mut sink = |_: Event| ();
+
+                let result = super::ProcessHandle::run(&mut sink, slice_of_strings!("/usr/bin/false"));
+                assert_eq!(true, result.is_ok());
+                assert_eq!(1i32, result.unwrap());
+            }
+
+            #[test]
+            fn exec_failure() {
+                let mut sink = |_: Event| ();
+
+                let result = super::ProcessHandle::run(&mut sink, slice_of_strings!("./path/to/not/exists"));
+                assert_eq!(false, result.is_ok());
+            }
+        }
+
+        mod test_events {
+            use super::*;
+            use std::env;
+            use std::process;
+            use nix::sys::signal;
+            use nix::unistd::Pid;
+
+            fn run_supervisor(args: &[String]) -> Vec<Event> {
+                let mut events: Vec<Event> = vec![];
+                {
+                    let _ = super::ProcessHandle::run(&mut |event: Event| {
+                        (&mut events).push(event);
+                    }, args);
+                }
+                events
+            }
+
+            fn assert_start_stop_events(args: &[String], expected_exit_code: i32) {
+                let events = run_supervisor(args);
+
+                assert_eq!(2usize, (&events).len());
+                // assert that the pid is not any of us.
+                assert_ne!(0, events[0].pid());
+                assert_ne!(process::id(), events[0].pid());
+                // assert that the all event's pid are the same.
+                assert_eq!(events[0].pid(), events[1].pid());
+                match events[0] {
+                    Event::Created { ppid, ref cwd, ref cmd, .. } => {
+                        assert_eq!(env::current_dir().unwrap().as_os_str(), cwd.as_os_str());
+                        assert_eq!(args.to_vec(), *cmd);
+                    },
+                    _ => assert_eq!(true, false),
+                }
+                match events[1] {
+                    Event::TerminatedNormally { code, .. } => {
+                        assert_eq!(expected_exit_code, code);
+                    },
+                    _ => assert_eq!(true, false),
+                }
+            }
+
+            #[test]
+            fn success() {
+                assert_start_stop_events(slice_of_strings!("/usr/bin/true"), 0i32);
+            }
+
+            #[test]
+            fn fail() {
+                assert_start_stop_events(slice_of_strings!("/usr/bin/false"), 1i32);
+            }
+
+            #[test]
+            fn exec_failure() {
+                let events = run_supervisor(slice_of_strings!("./path/to/not/exists"));
+                assert_eq!(0usize, (&events).len());
             }
         }
     }
