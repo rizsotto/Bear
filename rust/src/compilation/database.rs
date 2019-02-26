5e79e7bfa133806ef4dc793cea7f5bf926ccc6a8
Laszlo Nagy <rizsotto@gmail.com>
2019-02-26 12:32:20 +1100

    compilation database implemented

--- rust/src/compilation/database.rs
+++ rust/src/compilation/database.rs
@@ -18,13 +18,14 @@
  */
 
 use std::collections;
+use std::fs;
 use std::path;
 
 use Result;
 
 
 /// Represents a generic entry of the compilation database.
-#[derive(Hash)]
+#[derive(Hash, Debug)]
 pub struct Entry {
     pub directory: path::PathBuf,
     pub file: path::PathBuf,
@@ -48,24 +49,10 @@ type Entries = collections::HashSet<Entry>;
 
 /// Represents the expected format of the JSON compilation database.
 pub struct DatabaseFormat {
-    command_as_array: bool,
+    pub command_as_array: bool,
 
     // Other attributes might be:
-    // - output present or not
-    // - paths are relative or absolute
-}
-
-impl DatabaseFormat {
-    pub fn new() -> Self {
-        DatabaseFormat {
-            command_as_array: true,
-        }
-    }
-
-    pub fn command_as_array(&mut self, value: bool) -> &mut Self {
-        self.command_as_array = value;
-        self
-    }
+    // - output field dropped or preserved.
 }
 
 /// Represents a JSON compilation database.
@@ -78,12 +65,265 @@ impl Database {
         Database { path: path.to_path_buf(), }
     }
 
-    pub fn load() -> Result<Entries> {
-        unimplemented!()
+    pub fn load(&self) -> Result<Entries> {
+        let generic_entries = inner::load(&self.path)?;
+        let entries = generic_entries.iter()
+            .map(|entry| inner::into(entry))
+            .collect::<Result<Entries>>();
+        // In case of error, let's be verbose which entries were problematic.
+        if let Err(_) = entries {
+            let errors = generic_entries.iter()
+                .map(|entry| inner::into(entry))
+                .filter_map(Result::err)
+                .map(|error| error.to_string())
+                .collect::<Vec<_>>()
+                .join(", ");
+            Err(errors.into())
+        } else {
+            entries
+        }
     }
 
-    pub fn save(_entries: &Entries, _format: DatabaseFormat) -> Result<()> {
-        unimplemented!()
+    pub fn save(&self, entries: &Entries, format: &DatabaseFormat) -> Result<()> {
+        let generic_entries = entries.iter()
+            .map(|entry| inner::from(entry, format))
+            .collect::<Result<Vec<_>>>()?;
+        inner::save(&self.path, &generic_entries)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::{Read, Write};
+
+    macro_rules! vec_of_strings {
+        ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_load_not_existing_file_fails() {
+        let sut = Database::new(path::Path::new("/not/exists/file.json"));
+        let _ = sut.load().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_load_json_failed() {
+        let comp_db_file = TestFile::new()
+            .expect("test file setup failed");
+        comp_db_file.write(br#"this is not json"#)
+            .expect("test file content write failed");
+
+        let sut = Database::new(comp_db_file.path());
+        let _ = sut.load().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_load_not_expected_json_failed() {
+        let comp_db_file = TestFile::new()
+            .expect("test file setup failed");
+        comp_db_file.write(br#"{ "file": "string" }"#)
+            .expect("test file content write failed");
+
+        let sut = Database::new(comp_db_file.path());
+        let _ = sut.load().unwrap();
+    }
+
+    #[test]
+    fn test_load_empty() -> Result<()> {
+        let comp_db_file = TestFile::new()?;
+        comp_db_file.write(br#"[]"#)?;
+
+        let sut = Database::new(comp_db_file.path());
+        let entries = sut.load()?;
+
+        let expected = Entries::new();
+        assert_eq!(expected, entries);
+        Ok(())
+    }
+
+    #[test]
+    fn test_load_string_command() -> Result<()> {
+        let comp_db_file = TestFile::new()?;
+        comp_db_file.write(
+            br#"[
+                {
+                    "directory": "/home/user",
+                    "file": "./file_a.c",
+                    "command": "cc -c ./file_a.c -o ./file_a.o"
+                },
+                {
+                    "directory": "/home/user",
+                    "file": "./file_b.c",
+                    "output": "./file_b.o",
+                    "command": "cc -c ./file_b.c -o ./file_b.o"
+                }
+            ]"#
+        )?;
+
+        let sut = Database::new(comp_db_file.path());
+        let entries = sut.load()?;
+
+        let expected = expected_values();
+        assert_eq!(expected, entries);
+        Ok(())
+    }
+
+    #[test]
+    fn test_load_array_command() -> Result<()> {
+        let comp_db_file = TestFile::new()?;
+        comp_db_file.write(
+            br#"[
+                {
+                    "directory": "/home/user",
+                    "file": "./file_a.c",
+                    "arguments": ["cc", "-c", "./file_a.c", "-o", "./file_a.o"]
+                },
+                {
+                    "directory": "/home/user",
+                    "file": "./file_b.c",
+                    "output": "./file_b.o",
+                    "arguments": ["cc", "-c", "./file_b.c", "-o", "./file_b.o"]
+                }
+            ]"#
+        )?;
+
+        let sut = Database::new(comp_db_file.path());
+        let entries = sut.load()?;
+
+        let expected = expected_values();
+        assert_eq!(expected, entries);
+        Ok(())
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_load_path_problem() {
+        let comp_db_file = TestFile::new()
+            .expect("test file setup failed");
+        comp_db_file.write(br#"[
+                {
+                    "directory": " ",
+                    "file": "./file_a.c",
+                    "command": "cc -Dvalue=\"this"
+                }
+            ]"#)
+            .expect("test file content write failed");
+
+        let sut = Database::new(comp_db_file.path());
+        let _ = sut.load().unwrap();
+    }
+
+    #[test]
+    fn test_save_string_command() -> Result<()> {
+        let comp_db_file = TestFile::new()?;
+
+        let sut = Database::new(comp_db_file.path());
+        let formatter = DatabaseFormat { command_as_array: false };
+
+        let expected = expected_values();
+        sut.save(&expected, &formatter)?;
+
+        let entries = sut.load()?;
+
+        let expected = expected_values();
+        assert_eq!(expected, entries);
+
+        let content = comp_db_file.read()?;
+        println!("{}", content);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_save_array_command() -> Result<()> {
+        let comp_db_file = TestFile::new()?;
+
+        let sut = Database::new(comp_db_file.path());
+        let formatter = DatabaseFormat { command_as_array: true };
+
+        let expected = expected_values();
+        sut.save(&expected, &formatter)?;
+
+        let entries = sut.load()?;
+
+        let expected = expected_values();
+        assert_eq!(expected, entries);
+
+        let content = comp_db_file.read()?;
+        println!("{}", content);
+
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    struct TestFile {
+        directory: tempfile::TempDir,
+        file: path::PathBuf,
+    }
+
+    impl TestFile {
+
+        pub fn new() -> Result<TestFile> {
+            let directory = tempfile::Builder::new()
+                .prefix("bear-test-")
+                .rand_bytes(12)
+                .tempdir()?;
+
+            let mut file = directory.path().to_path_buf();
+            file.push("comp-db.json");
+
+            Ok(TestFile { directory, file })
+        }
+
+        pub fn path(&self) -> &path::Path {
+            self.file.as_path()
+        }
+
+        pub fn write(&self, content: &[u8]) -> Result<()> {
+            let mut file = fs::OpenOptions::new()
+                .write(true)
+                .truncate(true)
+                .create(true)
+                .open(self.path())?;
+
+            file.write(content)?;
+            Ok(())
+        }
+
+        pub fn read(&self) -> Result<String> {
+            let mut file = fs::OpenOptions::new()
+                .read(true)
+                .open(self.path())?;
+
+            let mut result = String::new();
+            file.read_to_string(&mut result)?;
+            Ok(result)
+        }
+    }
+
+    fn expected_values() -> Entries {
+        let mut expected: Entries = collections::HashSet::new();
+        expected.insert(
+            Entry {
+                directory: path::PathBuf::from("/home/user"),
+                file: path::PathBuf::from("./file_a.c"),
+                command: vec_of_strings!("cc", "-c", "./file_a.c", "-o", "./file_a.o"),
+                output: None,
+            }
+        );
+        expected.insert(
+            Entry {
+                directory: path::PathBuf::from("/home/user"),
+                file: path::PathBuf::from("./file_b.c"),
+                command: vec_of_strings!("cc", "-c", "./file_b.c", "-o", "./file_b.o"),
+                output: Some(path::PathBuf::from("./file_b.o")),
+            }
+        );
+        expected
     }
 }
 
@@ -91,115 +331,114 @@ impl Database {
 mod inner {
     use super::*;
     use serde_json;
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct StringEntry {
-        directory: String,
-        file: String,
-        command: String,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        output: Option<String>,
-    }
-
-    #[derive(Debug, Serialize, Deserialize)]
-    struct ArrayEntry {
-        directory: String,
-        file: String,
-        arguments: Vec<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        output: Option<String>,
-    }
+    use shellwords;
 
     #[derive(Debug, Serialize, Deserialize)]
     #[serde(untagged)]
-    enum GenericEntry {
-        StringEntry(StringEntry),
-        ArrayEntry(ArrayEntry),
+    pub enum GenericEntry {
+        StringEntry {
+            directory: String,
+            file: String,
+            command: String,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            output: Option<String>,
+        },
+        ArrayEntry {
+            directory: String,
+            file: String,
+            arguments: Vec<String>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            output: Option<String>,
+        },
     }
 
     type GenericEntries = Vec<GenericEntry>;
 
 
-    fn load(_path: &path::Path) -> Result<GenericEntries> {
-        unimplemented!()
+    pub fn load(path: &path::Path) -> Result<GenericEntries> {
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .open(path)?;
+        let entries: GenericEntries = serde_json::from_reader(file)?;
+        Ok(entries)
     }
 
-    fn save(_path: &path::Path, _entries: &GenericEntries) -> Result<()> {
-        unimplemented!()
+    pub fn save(path: &path::Path, entries: &GenericEntries) -> Result<()> {
+        let file = fs::OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(path)?;
+        serde_json::ser::to_writer_pretty(file, entries)
+            .map_err(|error| error.into())
     }
 
-    fn from(_entry: Entry, _format: DatabaseFormat) -> GenericEntry {
-        unimplemented!()
-    }
-
-    fn into(_entry: GenericEntry) -> Entry {
-        unimplemented!()
-    }
-
-    #[cfg(test)]
-    mod test {
-        use super::*;
-
-        #[test]
-        fn test_load_arguments() {
-            let input =
-                r#"{
-                "directory": "/build/dir/path",
-                "file": "/path/to/source/file.c",
-                "arguments": ["cc", "-c", "/path/to/source/file.c"]
-            }"#;
-
-            let entry: GenericEntry = serde_json::from_str(input).unwrap();
-            println!("{:?}", entry);
+    pub fn from(entry: &Entry, format: &DatabaseFormat) -> Result<GenericEntry> {
+        fn path_to_string(path: &path::Path) -> Result<String> {
+            match path.to_str() {
+                Some(str) => Ok(str.to_string()),
+                None => Err(format!("Failed to convert to string {:?}", path).into()),
+            }
         }
 
-        #[test]
-        fn test_save_arguments() {
-            let entry_one = GenericEntry::ArrayEntry(ArrayEntry {
-                directory: "/build/dir/path".to_string(),
-                file: "/path/to/source.c".to_string(),
-                arguments: vec!["cc".to_string(), "-c".to_string()],
-                output: None
-            });
-            let entry_two = GenericEntry::StringEntry(StringEntry {
-                directory: "/build/dir/path".to_string(),
-                file: "/path/to/source.c".to_string(),
-                command: "cc -c /path/to/source.c -o /build/dir/path/source.o".to_string(),
-                output: Some("/build/dir/path/source.o".to_string())
-            });
-            let inputs = vec![entry_one, entry_two];
+        let directory = path_to_string(entry.directory.as_path())?;
+        let file = path_to_string(entry.file.as_path())?;
+        let output = match entry.output {
+            Some(ref path) => path_to_string(path).map(Option::Some),
+            None => Ok(None),
+        }?;
+        if format.command_as_array {
+            Ok(GenericEntry::ArrayEntry {
+                directory,
+                file,
+                arguments: entry.command.clone(),
+                output
+            })
+        } else {
+            Ok(GenericEntry::StringEntry {
+                directory,
+                file,
+                command: shellwords::join(
+                    entry.command
+                        .iter()
+                        .map(String::as_str)
+                        .collect::<Vec<_>>()
+                        .as_ref()),
+                output
+            })
+        }
+    }
 
-            let output = serde_json::to_string(&inputs).unwrap();
-            println!("{}", output);
+    pub fn into(entry: &GenericEntry) -> Result<Entry> {
+        match entry {
+            GenericEntry::ArrayEntry { directory, file, arguments, output} => {
+                let directory_path = path::PathBuf::from(directory);
+                let file_path = path::PathBuf::from(file);
+                let output_path = output.clone().map(|string| path::PathBuf::from(string));
+                Ok(Entry {
+                    directory: directory_path,
+                    file: file_path,
+                    command: arguments.clone(),
+                    output: output_path,
+                })
+            },
+            GenericEntry::StringEntry { directory, file, command, output } => {
+                match shellwords::split(command) {
+                    Ok(arguments ) => {
+                        let directory_path = path::PathBuf::from(directory);
+                        let file_path = path::PathBuf::from(file);
+                        let output_path = output.clone().map(|string| path::PathBuf::from(string));
+                        Ok(Entry {
+                            directory: directory_path,
+                            file: file_path,
+                            command: arguments,
+                            output: output_path,
+                        })
+                    },
+                    Err(_) =>
+                        Err(format!("Quotes are mismatch in {:?}", command).into()),
+                }
+            }
         }
     }
 }
-
-//impl Database {
-//    pub fn new() -> Database {
-//        Database {
-//            entries: collections::HashSet::new(),
-//        }
-//    }
-//
-//    pub fn load(&mut self, source: &mut io::Read) -> Result<()> {
-//        let entries: Vec<Entry> = serde_json::from_reader(source)?;
-//        let result = self.add_entries(entries);
-//        Ok(result)
-//    }
-//
-//    pub fn save(&self, target: &mut io::Write) -> Result<()> {
-//        let values = Vec::from_iter(self.entries.iter());
-//        let result = serde_json::to_writer(target, &values)?;
-//        Ok(result)
-//    }
-//
-//    pub fn add_entry(&mut self, entry: Entry) -> () {
-//        self.entries.insert(entry);
-//    }
-//
-//    pub fn add_entries(&mut self, entries: Vec<Entry>) -> () {
-//        let fresh: collections::HashSet<Entry> = collections::HashSet::from_iter(entries);
-//        self.entries.union(&fresh);
-//    }
-//}
