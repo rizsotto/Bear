4a2bd6bbdd4aa09e492112fa7f871162663756f6
Laszlo Nagy <rizsotto@gmail.com>
2020-04-05 17:00:53 +1000

    implement session environment update method

--- source/intercept/source/Session.cc
+++ source/intercept/source/Session.cc
@@ -17,16 +17,80 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Session.h"
-
 #include "config.h"
+
+#include "Session.h"
+#include "Application.h"
+#include "libexec.h"
 
 #ifdef HAVE_SYS_UTSNAME_H
 #include <sys/utsname.h>
 #endif
+#include <functional>
+#include <numeric>
 #include <unistd.h>
 
+#include <spdlog/spdlog.h>
+
 namespace {
+
+    constexpr char GLIBC_PRELOAD_KEY[] = "LD_PRELOAD";
+
+    using env_t = std::map<std::string, std::string>;
+    using mapper_t = std::function<std::string(const std::string&, const std::string&)>;
+
+    std::list<std::string>
+    split(const std::string& input, const char sep) noexcept
+    {
+        std::list<std::string> result;
+
+        std::string::size_type previous = 0;
+        do {
+            const std::string::size_type current = input.find(sep, previous);
+            result.emplace_back(input.substr(previous, current - previous));
+            previous = (current != std::string::npos) ? current + 1 : current;
+        } while (previous != std::string::npos);
+
+        return result;
+    }
+
+    std::string
+    merge_into_paths(const std::string& current, const std::string& value) noexcept
+    {
+        auto paths = split(current, ':');
+        if (std::find(paths.begin(), paths.end(), value) == paths.end()) {
+            paths.emplace_front(value);
+            return std::accumulate(paths.begin(),
+                                   paths.end(),
+                                   std::string(),
+                                   [](std::string acc, std::string item) {
+                                       return (acc.empty()) ? item : acc + ':' + item;
+                                   });
+        } else {
+            return current;
+        }
+    }
+
+    void insert_or_assign(env_t& target, const char* key, const std::string& value) noexcept
+    {
+        if (auto it = target.find(key); it != target.end()) {
+            it->second = value;
+        } else {
+            target.emplace(key, value);
+        }
+    }
+
+    void insert_or_merge(env_t& target,
+                         const char* key,
+                         const std::string& value,
+                         const mapper_t& merger) noexcept
+    {
+        if (auto it = target.find(key); it != target.end()) {
+            it->second = merger(it->second, value);
+        } else {
+            target.emplace(key, value);
+        }
+    }
 
 #ifdef HAVE_CONFSTR
     rust::Result<std::string> get_confstr(const int key)
@@ -77,17 +141,17 @@ namespace {
 #endif
 #endif
         return (result.empty())
-            ? rust::Result<ic::Session::HostInfo>(rust::Err(std::runtime_error("not implemented")))
+            ? rust::Result<ic::Session::HostInfo>(rust::Err(std::runtime_error("failed to get host info.")))
             : rust::Result<ic::Session::HostInfo>(rust::Ok(result));
     }
 
     class LibraryPreloadSession : public ic::Session {
     public:
-        explicit LibraryPreloadSession(HostInfo &&host_info);
+        LibraryPreloadSession(HostInfo &&host_info, const std::string_view& library, const std::string_view& executor);
 
     public:
         [[nodiscard]] rust::Result<std::string_view> resolve(const std::string& name) const override;
-        [[nodiscard]] rust::Result<std::map<std::string, std::string>> update(const std::map<std::string, std::string>& env) const override;
+        [[nodiscard]] rust::Result<std::map<std::string, std::string>> update(const env_t& env) const override;
         [[nodiscard]] rust::Result<int> supervise(const std::vector<std::string_view>& command) const override;
 
         void set_server_address(const std::string&) override;
@@ -98,11 +162,15 @@ namespace {
     private:
         HostInfo host_info_;
         std::string server_address_;
+        std::string library_;
+        std::string executor_;
     };
 
-    LibraryPreloadSession::LibraryPreloadSession(ic::Session::HostInfo&& host_info)
+    LibraryPreloadSession::LibraryPreloadSession(ic::Session::HostInfo&& host_info, const std::string_view& library, const std::string_view& executor)
             : host_info_(host_info)
             , server_address_()
+            , library_(library)
+            , executor_(executor)
     {
     }
 
@@ -111,10 +179,15 @@ namespace {
         return rust::Err(std::runtime_error("The session does not support resolve."));
     }
 
-    rust::Result<std::map<std::string, std::string>> LibraryPreloadSession::update(const std::map<std::string, std::string>& env) const
+    rust::Result<std::map<std::string, std::string>> LibraryPreloadSession::update(const env_t& env) const
     {
-        // TODO
-        return rust::Ok(env);
+        env_t copy(env);
+        insert_or_assign(copy, el::env::KEY_REPORTER, executor_);
+        insert_or_assign(copy, el::env::KEY_DESTINATION, server_address_);
+        insert_or_assign(copy, el::env::KEY_LIBRARY, library_);
+        insert_or_merge(copy, GLIBC_PRELOAD_KEY, library_, merge_into_paths);
+
+        return rust::Ok(copy);
     }
 
     rust::Result<int> LibraryPreloadSession::supervise(const std::vector<std::string_view>& command) const
@@ -135,7 +208,7 @@ namespace {
 
     std::string LibraryPreloadSession::get_session_type() const
     {
-        return std::string("library_preload");
+        return std::string("library preload");
     }
 }
 
@@ -143,9 +216,20 @@ namespace ic {
 
     rust::Result<Session::SharedPtr> Session::from(const flags::Arguments& args)
     {
-        return create_host_info()
-                .map<Session::SharedPtr>([](auto info) {
-                    return std::shared_ptr<Session>(new LibraryPreloadSession(std::move(info)));
+        auto host_info = create_host_info()
+            .unwrap_or_else([](auto error) {
+                spdlog::info(error.what());
+                return std::map<std::string, std::string>();
+            });
+
+        auto library = args.as_string(ic::Application::LIBRARY);
+        auto executor = args.as_string(ic::Application::EXECUTOR);
+
+        return merge(library, executor)
+                .map<Session::SharedPtr>([&host_info](auto pair) {
+                    const auto& [library, executor] = pair;
+                    auto result = new LibraryPreloadSession(std::move(host_info), library, executor);
+                    return std::shared_ptr<Session>(result);
                 });
     }
 }
