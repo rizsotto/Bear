efd11441fe45083c4f75803d418d1a3ae5df95cd
Laszlo Nagy <rizsotto@gmail.com>
2020-06-24 22:55:57 +1000

    change functional test script to forward signal

--- test/cases/intercept/preload/signal_outside_build.sh
+++ test/cases/intercept/preload/signal_outside_build.sh
@@ -2,9 +2,9 @@
 
 # REQUIRES: preload, shell, dynamic-shell
 # RUN: %{shell} -c "%{intercept} --verbose --output %t.json -- %{shell} %s --sleep %{sleep} --true %{true} & %{sleep} 1; kill -15 %1; wait;"
-# RUN: assert_intercepted %t.json count -ge 2
+# RUN: assert_intercepted %t.json count -eq 3
 # RUN: assert_intercepted %t.json contains -program %{true}
-# RUN: assert_intercepted %t.json contains -program %{sleep}
+# RUN: assert_intercepted %t.json contains -program %{sleep} -arguments %{sleep} 5
 
 for i in "$@"
 do
@@ -38,6 +38,15 @@ if [ -z "$TRUE" ]; then
   exit 1;
 fi
 
+forward() {
+  kill -15 $child;
+}
+
+trap forward SIGTERM
+
 # do the test
 $TRUE
-$SLEEP 5
+$SLEEP 5&
+
+child=$!
+wait $child
