80eb9e42eb469f30cb633c5589917bd033ba1f44
Laszlo Nagy <rizsotto@gmail.com>
2021-11-07 09:57:44 +1100

    test: added against libtool

---
+++ test/cases/intercept/wrapper/libtool_commands_captured.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# REQUIRES: shell, libtool
+# RUN: %{intercept} --force-wrapper --verbose --output %t.json -- %{shell} %s
+# RUN: assert_intercepted %t.json count -ge 6
+# RUN: assert_intercepted %t.json contains -program %{c_compiler} -arguments %{c_compiler} -g -O -c main.c -o main.o
+# RUN: assert_intercepted %t.json contains -program %{c_compiler} -arguments %{c_compiler} -g -O -c hello.c -o hello.o
+
+echo "int main() { return 0; }" > main.c
+echo "int hello() { return 1; }" > hello.c
+
+$LIBTOOL --mode=compile --tag=CC $CC -g -O -c main.c -o main.o;
+$LIBTOOL --mode=compile --tag=CC $CC -g -O -c hello.c -o hello.o;
+$LIBTOOL --mode=link --tag=CC $CC -g -O -o libhello.la hello.lo
+$LIBTOOL --mode=link --tag=CC $CC -g -O -o libtool_test main.lo libhello.la
