6e737c194fe575f8e00c86348a3a73e02dab3fc9
Laszlo Nagy <rizsotto@gmail.com>
2024-11-16 21:10:10 +1100

    rust: bear modes are represented in types

--- rust/bear/src/bin/bear.rs
+++ rust/bear/src/bin/bear.rs
@@ -43,27 +43,38 @@ fn main() -> anyhow::Result<ExitCode> {
 
 /// Represent the application state.
 enum Application {
-    /// The intercept mode we are only capturing the build commands.
-    Intercept {
-        input: args::BuildCommand,
-        output: args::BuildEvents,
-        config: config::Intercept,
-    },
-    /// The semantic mode we are deduct the semantic meaning of the
-    /// executed commands from the build process.
-    Semantic {
-        event_source: EventFileReader,
-        semantic_recognition: Recognition,
-        semantic_transform: Transformation,
-        output_writer: OutputWriter,
-    },
-    /// The all model is combining the intercept and semantic modes.
-    All {
-        input: args::BuildCommand,
-        output: args::BuildSemantic,
-        intercept_config: config::Intercept,
-        output_config: config::Output,
-    },
+    Intercept(Intercept),
+    Semantic(Semantic),
+    All(All),
+}
+
+/// The intercept mode we are only capturing the build commands.
+struct Intercept {
+    input: args::BuildCommand,
+    output: args::BuildEvents,
+    config: config::Intercept,
+}
+
+/// The semantic mode we are deduct the semantic meaning of the
+/// executed commands from the build process.
+struct Semantic {
+    event_source: EventFileReader,
+    semantic_recognition: Recognition,
+    semantic_transform: Transformation,
+    output_writer: OutputWriter,
+}
+
+/// The all model is combining the intercept and semantic modes.
+struct All {
+    input: args::BuildCommand,
+    output: args::BuildSemantic,
+    intercept_config: config::Intercept,
+    output_config: config::Output,
+}
+
+/// The mode trait is used to run the application in different modes.
+trait Mode {
+    fn run(self) -> ExitCode;
 }
 
 impl Application {
@@ -76,107 +87,107 @@ impl Application {
         match args.mode {
             args::Mode::Intercept { input, output } => {
                 let intercept_config = config.intercept;
-                let result = Application::Intercept {
+                let mode = Intercept {
                     input,
                     output,
                     config: intercept_config,
                 };
-                Ok(result)
+                Ok(Application::Intercept(mode))
             }
             args::Mode::Semantic { input, output } => {
                 let event_source = EventFileReader::try_from(input)?;
                 let semantic_recognition = Recognition::try_from(&config)?;
                 let semantic_transform = Transformation::from(&config.output);
                 let output_writer = OutputWriter::configure(&output, &config.output)?;
-                let result = Application::Semantic {
+                let mode = Semantic {
                     event_source,
                     semantic_recognition,
                     semantic_transform,
                     output_writer,
                 };
-                Ok(result)
+                Ok(Application::Semantic(mode))
             }
             args::Mode::All { input, output } => {
                 let intercept_config = config.intercept;
                 let output_config = config.output;
-                let result = Application::All {
+                let mode = All {
                     input,
                     output,
                     intercept_config,
                     output_config,
                 };
-                Ok(result)
+                Ok(Application::All(mode))
             }
         }
     }
+}
 
-    /// Executes the configured application.
+impl Mode for Application {
     fn run(self) -> ExitCode {
         match self {
-            Application::Intercept {
-                input,
-                output,
-                config,
-            } => {
-                match &config {
-                    config::Intercept::Wrapper { .. } => {
-                        let service = InterceptService::new()
-                            .expect("Failed to create the intercept service");
-                        let environment = InterceptEnvironment::new(&config, service.address())
-                            .expect("Failed to create the intercept environment");
+            Application::Intercept(intercept) => intercept.run(),
+            Application::Semantic(semantic) => semantic.run(),
+            Application::All(all) => all.run(),
+        }
+    }
+}
 
-                        // start writer thread
-                        let writer_thread = thread::spawn(move || {
-                            let mut writer = std::fs::File::create(output.file_name)
-                                .expect("Failed to create the output file");
-                            for envelope in service.receiver().iter() {
-                                envelope
-                                    .write_into(&mut writer)
-                                    .expect("Failed to write the envelope");
-                            }
-                        });
+impl Mode for Intercept {
+    fn run(self) -> ExitCode {
+        match &self.config {
+            config::Intercept::Wrapper { .. } => {
+                let service = InterceptService::new()
+                    .expect("Failed to create the intercept service");
+                let environment = InterceptEnvironment::new(&self.config, service.address())
+                    .expect("Failed to create the intercept environment");
 
-                        let status = environment.execute_build_command(input);
-
-                        writer_thread
-                            .join()
-                            .expect("Failed to join the writer thread");
-
-                        status.unwrap_or(ExitCode::FAILURE)
+                // start writer thread
+                let writer_thread = thread::spawn(move || {
+                    let mut writer = std::fs::File::create(self.output.file_name)
+                        .expect("Failed to create the output file");
+                    for envelope in service.receiver().iter() {
+                        envelope
+                            .write_into(&mut writer)
+                            .expect("Failed to write the envelope");
                     }
-                    config::Intercept::Preload { .. } => {
-                        todo!()
-                    }
-                }
+                });
+
+                let status = environment.execute_build_command(self.input);
+
+                writer_thread
+                    .join()
+                    .expect("Failed to join the writer thread");
+
+                status.unwrap_or(ExitCode::FAILURE)
             }
-            Application::Semantic {
-                event_source,
-                semantic_recognition,
-                semantic_transform,
-                output_writer,
-            } => {
-                // Set up the pipeline of compilation database entries.
-                let entries = event_source
-                    .generate()
-                    .flat_map(|execution| semantic_recognition.apply(execution))
-                    .flat_map(|semantic| semantic_transform.apply(semantic));
-                // Consume the entries and write them to the output file.
-                // The exit code is based on the result of the output writer.
-                match output_writer.run(entries) {
-                    Ok(_) => ExitCode::SUCCESS,
-                    Err(_) => ExitCode::FAILURE,
-                }
-            }
-            Application::All {
-                input,
-                output,
-                intercept_config,
-                output_config,
-            } => {
-                // TODO: Implement the all mode.
-                ExitCode::FAILURE
+            config::Intercept::Preload { .. } => {
+                todo!()
             }
         }
+    }
+}
+
+impl Mode for Semantic {
+    fn run(self) -> ExitCode {
+        // Set up the pipeline of compilation database entries.
+        let entries = self
+            .event_source
+            .generate()
+            .flat_map(|execution| self.semantic_recognition.apply(execution))
+            .flat_map(|semantic| self.semantic_transform.apply(semantic));
+        // Consume the entries and write them to the output file.
+        // The exit code is based on the result of the output writer.
+        match self.output_writer.run(entries) {
+            Ok(_) => ExitCode::SUCCESS,
+            Err(_) => ExitCode::FAILURE,
+        }
+    }
+}
+
+impl Mode for All {
+    fn run(self) -> ExitCode {
+        // TODO: Implement the all mode.
+        ExitCode::FAILURE
     }
 }
 
