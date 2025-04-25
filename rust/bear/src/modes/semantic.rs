9e55969ef6ab7467a01e505467864b8052ea4967
Laszlo Nagy <rizsotto@gmail.com>
2025-04-25 20:06:00 +1000

    rust: clang output writer simplified

--- rust/bear/src/modes/semantic.rs
+++ rust/bear/src/modes/semantic.rs
@@ -1,54 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use crate::intercept::Event;
-use crate::output::OutputWriter;
+use crate::output::IteratorWriter;
 use crate::semantic::interpreters;
 use crate::semantic::transformation::FilterAndFormat;
 use crate::semantic::{transformation, Recognition};
 use crate::{args, config, output, semantic};
-use anyhow::Context;
-use std::fs::{File, OpenOptions};
-use std::io::{BufReader, BufWriter};
-use std::path::{Path, PathBuf};
 
-/// The semantic analysis that is independent of the event source.
-pub(super) struct SemanticAnalysisPipeline {
+pub(super) struct SemanticAnalysis {
     interpreter: Box<dyn semantic::Interpreter>,
     transformation: Box<dyn transformation::Transformation>,
-    output_writer: OutputWriterImpl,
 }
 
-impl SemanticAnalysisPipeline {
-    /// Create a new semantic mode instance.
-    pub(super) fn create(
-        output: args::BuildSemantic,
-        config: &config::Main,
-    ) -> anyhow::Result<Self> {
+impl TryFrom<&config::Main> for SemanticAnalysis {
+    type Error = anyhow::Error;
+
+    fn try_from(config: &config::Main) -> Result<Self, Self::Error> {
         let interpreter = interpreters::create(config);
         let transformation = FilterAndFormat::try_from(&config.output)?;
-        let output_writer = OutputWriterImpl::create(&output, &config.output)?;
 
         Ok(Self {
             interpreter: Box::new(interpreter),
             transformation: Box::new(transformation),
-            output_writer,
         })
     }
+}
 
-    /// Consumer the envelopes for analysis and write the result to the output file.
-    /// This implements the pipeline of the semantic analysis.
-    pub(super) fn analyze_and_write(
-        self,
-        events: impl IntoIterator<Item = Event>,
-    ) -> anyhow::Result<()> {
-        // Set up the pipeline of compilation database entries.
-        let semantics = events.into_iter().flat_map(|event| self.analyze(event));
-        // Consume the entries and write them to the output file.
-        // The exit code is based on the result of the output writer.
-        self.output_writer.run(semantics)
-    }
-
-    pub(super) fn analyze(&self, event: Event) -> Option<semantic::CompilerCall> {
+impl SemanticAnalysis {
+    pub fn analyze(&self, event: Event) -> Option<semantic::CompilerCall> {
         log::debug!("event: {}", event);
         match self.interpreter.recognize(&event.execution) {
             Recognition::Success(recognized) => {
@@ -89,164 +68,36 @@ impl SemanticAnalysisPipeline {
     }
 }
 
-/// The output writer implementation.
-///
-/// This is a workaround for the lack of trait object support for generic arguments.
-/// https://doc.rust-lang.org/reference/items/traits.html#object-safety.
-pub(crate) enum OutputWriterImpl {
-    Clang(ClangOutputWriter),
-    Semantic(SemanticOutputWriter),
+/// The semantic analysis that is independent of the event source.
+pub(super) struct SemanticAnalysisPipeline {
+    analyzer: SemanticAnalysis,
+    writer: output::OutputWriter,
 }
 
-impl OutputWriter for OutputWriterImpl {
-    fn run(&self, semantics: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
-        match self {
-            OutputWriterImpl::Clang(writer) => writer.run(semantics),
-            OutputWriterImpl::Semantic(writer) => writer.run(semantics),
-        }
+impl SemanticAnalysisPipeline {
+    /// Create a new semantic mode instance.
+    pub(super) fn create(
+        output: args::BuildSemantic,
+        config: &config::Main,
+    ) -> anyhow::Result<Self> {
+        let analyzer = SemanticAnalysis::try_from(config)?;
+        let writer = output::OutputWriter::try_from((&output, &config.output))?;
+
+        Ok(Self { analyzer, writer })
     }
-}
 
-impl OutputWriterImpl {
-    /// Create a new instance of the output writer.
-    pub(crate) fn create(
-        args: &args::BuildSemantic,
-        config: &config::Output,
-    ) -> anyhow::Result<OutputWriterImpl> {
-        // TODO: This method should fail early if the output file is not writable.
-        match config {
-            config::Output::Clang { duplicates, .. } => {
-                let result = ClangOutputWriter {
-                    output: PathBuf::from(&args.file_name),
-                    append: args.append,
-                    formatter: output::formatter::EntryFormatter::new(),
-                    filter: output::filter_duplicates::DuplicateFilter::try_from(
-                        duplicates.clone(),
-                    )?,
-                };
-                Ok(OutputWriterImpl::Clang(result))
-            }
-            config::Output::Semantic { .. } => {
-                let result = SemanticOutputWriter {
-                    output: PathBuf::from(&args.file_name),
-                };
-                Ok(OutputWriterImpl::Semantic(result))
-            }
-        }
-    }
-}
-
-pub(crate) struct SemanticOutputWriter {
-    output: PathBuf,
-}
-
-impl OutputWriter for SemanticOutputWriter {
-    fn run(&self, semantics: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
-        let file_name = &self.output;
-        let file = File::create(file_name)
-            .map(BufWriter::new)
-            .with_context(|| format!("Failed to create file: {:?}", file_name.as_path()))?;
-
-        semantic::serialize(file, semantics)?;
-
-        Ok(())
-    }
-}
-
-/// Responsible for writing the final compilation database file.
-///
-/// Implements filtering, formatting and atomic file writing.
-/// (Atomic file writing implemented by writing to a temporary file and renaming it.)
-///
-/// Filtering is implemented by the `filter` module, and the formatting is implemented by the
-/// `json_compilation_db` module.
-pub(crate) struct ClangOutputWriter {
-    output: PathBuf,
-    append: bool,
-    formatter: output::formatter::EntryFormatter,
-    filter: output::filter_duplicates::DuplicateFilter,
-}
-
-impl OutputWriter for ClangOutputWriter {
-    /// Implements the main logic of the output writer.
-    fn run(&self, semantics: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
-        let entries = semantics.flat_map(|semantic| self.formatter.apply(semantic));
-        if self.append && self.output.exists() {
-            let entries_from_db = Self::read_from_compilation_db(self.output.as_path())?;
-            let final_entries = entries_from_db.chain(entries);
-            self.write_into_compilation_db(final_entries)
-        } else {
-            if self.append {
-                log::warn!("The output file does not exist, the append option is ignored.");
-            }
-            self.write_into_compilation_db(entries)
-        }
-    }
-}
-
-impl ClangOutputWriter {
-    /// Write the entries to the compilation database.
-    ///
-    /// The entries are written to a temporary file and then renamed to the final output.
-    /// This ensures that the output file is always in a consistent state.
-    fn write_into_compilation_db(
-        &self,
-        entries: impl Iterator<Item = output::clang::Entry>,
+    /// Consumer the envelopes for analysis and write the result to the output file.
+    /// This implements the pipeline of the semantic analysis.
+    pub(super) fn analyze_and_write(
+        self,
+        events: impl IntoIterator<Item = Event>,
     ) -> anyhow::Result<()> {
-        // Filter out the entries as per the configuration.
-        let mut filter = self.filter.clone();
-        let filtered_entries = entries.filter(move |entry| filter.unique(entry));
-        // Write the entries to a temporary file.
-        self.write_into_temporary_compilation_db(filtered_entries)
-            .and_then(|temp| {
-                // Rename the temporary file to the final output.
-                std::fs::rename(temp.as_path(), self.output.as_path()).with_context(|| {
-                    format!(
-                        "Failed to rename file from '{:?}' to '{:?}'.",
-                        temp.as_path(),
-                        self.output.as_path()
-                    )
-                })
-            })
-    }
-
-    /// Write the entries to a temporary file and returns the temporary file name.
-    fn write_into_temporary_compilation_db(
-        &self,
-        entries: impl Iterator<Item = output::clang::Entry>,
-    ) -> anyhow::Result<PathBuf> {
-        // Generate a temporary file name.
-        let file_name = self.output.with_extension("tmp");
-        // Open the file for writing.
-        let file = File::create(&file_name)
-            .map(BufWriter::new)
-            .with_context(|| format!("Failed to create file: {:?}", file_name.as_path()))?;
-        // Write the entries to the file.
-        output::clang::write(file, entries)
-            .with_context(|| format!("Failed to write entries: {:?}", file_name.as_path()))?;
-        // Return the temporary file name.
-        Ok(file_name)
-    }
-
-    /// Read the compilation database from a file.
-    fn read_from_compilation_db(
-        source: &Path,
-    ) -> anyhow::Result<impl Iterator<Item = output::clang::Entry>> {
-        let source_copy = source.to_path_buf();
-
-        let file = OpenOptions::new()
-            .read(true)
-            .open(source)
-            .map(BufReader::new)
-            .with_context(|| format!("Failed to open file: {:?}", source))?;
-
-        let entries = output::clang::read(file).filter_map(move |candidate| match candidate {
-            Ok(entry) => Some(entry),
-            Err(error) => {
-                log::error!("Failed to read file: {:?}, reason: {}", source_copy, error);
-                None
-            }
-        });
-        Ok(entries)
+        // Set up the pipeline of compilation database entries.
+        let semantics = events
+            .into_iter()
+            .flat_map(|event| self.analyzer.analyze(event));
+        // Consume the entries and write them to the output file.
+        // The exit code is based on the result of the output writer.
+        self.writer.write(semantics)
     }
 }
--- rust/bear/src/output/mod.rs
+++ rust/bear/src/output/mod.rs
@@ -1,15 +1,291 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use super::semantic;
-use anyhow::Result;
+//! This module is responsible for writing the output of the semantic analysis.
+//!
+//! The output can be in different formats, such as JSON compilation databases
+//! or semantic analysis results in JSON format. The module provides functionality
+//! to write these outputs to files, handle duplicates, and format the output
+//! as needed.
+//!
+//! The public API of this module includes the `IteratorWriter` trait, which is
+//! implemented by different output writers. The `OutputWriter` enum represents
+//! the main entry point for writing the output. The input of the `OutputWriter`
+//! is a stream of `semantic::CompilerCall` instances.
 
 pub mod clang;
 pub mod filter_duplicates;
 pub mod formatter;
 
-/// The output writer trait is responsible for writing output file.
-pub(crate) trait OutputWriter {
-    /// Running the writer means to consume the compiler calls
-    /// and write the entries to the output file.
-    fn run(&self, _: impl Iterator<Item = semantic::CompilerCall>) -> Result<()>;
+use super::{args, config, semantic};
+use anyhow::Context;
+use serde::ser::SerializeSeq;
+use serde::Serializer;
+use std::{fs, io, path};
+
+/// The trait represents a writer for iterator type `T`.
+///
+/// This trait is implemented by types that can consume an iterator of type `T`
+/// and write its elements to some output. The writing process may succeed or fail,
+/// returning either `()` on success or an error.
+pub trait IteratorWriter<T> {
+    /// Writes the iterator as a sequence of elements.
+    /// It consumes the iterator and returns either a nothing or an error.
+    fn write(self, _: impl Iterator<Item = T>) -> anyhow::Result<()>;
+}
+
+/// Represents the output writer, which can handle different types of outputs.
+///
+/// This enum provides two variants:
+/// - `Clang`: Handles output for Clang compilation databases.
+/// - `Semantic`: Handles output for semantic analysis results.
+///
+/// The specific behavior of each variant is implemented in their respective types.
+pub enum OutputWriter {
+    #[allow(private_interfaces)]
+    Clang(FormattedClangOutputWriter),
+    #[allow(private_interfaces)]
+    Semantic(SemanticOutputWriter),
+}
+
+impl TryFrom<(&args::BuildSemantic, &config::Output)> for OutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(value: (&args::BuildSemantic, &config::Output)) -> Result<Self, Self::Error> {
+        let (args, config) = value;
+        match config {
+            config::Output::Clang { duplicates, .. } => {
+                let result = FormattedClangOutputWriter::try_from((args, duplicates))?;
+                Ok(Self::Clang(result))
+            }
+            config::Output::Semantic { .. } => {
+                let path = path::Path::new(&args.file_name);
+                let result = SemanticOutputWriter::try_from(path)?;
+                Ok(Self::Semantic(result))
+            }
+        }
+    }
+}
+
+impl IteratorWriter<semantic::CompilerCall> for OutputWriter {
+    fn write(self, semantics: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
+        match self {
+            Self::Clang(writer) => writer.write(semantics),
+            Self::Semantic(writer) => writer.write(semantics),
+        }
+    }
+}
+
+/// This writer is used to write the semantic analysis results to a file.
+///
+/// # Note
+/// The output format is not stable and may change in future versions.
+/// It reflects the internal representation of the semantic analysis types.
+struct SemanticOutputWriter {
+    output: io::BufWriter<fs::File>,
+}
+
+impl TryFrom<&path::Path> for SemanticOutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(file_name: &path::Path) -> Result<Self, Self::Error> {
+        let output = fs::File::create(file_name)
+            .map(io::BufWriter::new)
+            .with_context(|| format!("Failed to open file: {:?}", file_name))?;
+
+        Ok(Self { output })
+    }
+}
+
+impl IteratorWriter<semantic::CompilerCall> for SemanticOutputWriter {
+    // FIXME: this is the same method as `clang::write` for entries. Should be generalized?
+    fn write(self, entries: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
+        let mut ser = serde_json::Serializer::pretty(self.output);
+        let mut seq = ser.serialize_seq(None)?;
+        for entry in entries {
+            seq.serialize_element(&entry)?;
+        }
+        seq.end()?;
+
+        Ok(())
+    }
+}
+
+/// Formats `semantic::CompilerCall` instances into `clang::Entry` objects.
+struct FormattedClangOutputWriter {
+    formatter: formatter::EntryFormatter,
+    writer: AppendClangOutputWriter,
+}
+
+impl TryFrom<(&args::BuildSemantic, &config::DuplicateFilter)> for FormattedClangOutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        value: (&args::BuildSemantic, &config::DuplicateFilter),
+    ) -> Result<Self, Self::Error> {
+        let (args, config) = value;
+
+        let formatter = formatter::EntryFormatter::new();
+        let writer = AppendClangOutputWriter::try_from((args, config))?;
+
+        Ok(Self { formatter, writer })
+    }
+}
+
+impl IteratorWriter<semantic::CompilerCall> for FormattedClangOutputWriter {
+    fn write(self, semantics: impl Iterator<Item = semantic::CompilerCall>) -> anyhow::Result<()> {
+        let entries = semantics.flat_map(|semantic| self.formatter.apply(semantic));
+        self.writer.write(entries)
+    }
+}
+
+/// Handles the logic for appending entries to an existing Clang output file.
+///
+/// This writer supports reading existing entries from a compilation database file,
+/// combining them with new entries, and writing the result back to the file.
+/// If the file does not exist and the append option is enabled, it logs a warning
+/// and writes only the new entries.
+struct AppendClangOutputWriter {
+    writer: AtomicClangOutputWriter,
+    path: Option<path::PathBuf>,
+}
+
+impl TryFrom<(&args::BuildSemantic, &config::DuplicateFilter)> for AppendClangOutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        value: (&args::BuildSemantic, &config::DuplicateFilter),
+    ) -> Result<Self, Self::Error> {
+        let (args, config) = value;
+
+        let file_name = path::Path::new(&args.file_name);
+        let path = if file_name.exists() {
+            Some(file_name.to_path_buf())
+        } else {
+            if args.append {
+                log::warn!("The output file does not exist, the append option is ignored.");
+            }
+            None
+        };
+
+        let writer = AtomicClangOutputWriter::try_from((file_name, config))?;
+
+        Ok(Self { writer, path })
+    }
+}
+
+impl IteratorWriter<clang::Entry> for AppendClangOutputWriter {
+    fn write(self, entries: impl Iterator<Item = clang::Entry>) -> anyhow::Result<()> {
+        if let Some(path) = self.path {
+            let entries_from_db = Self::read_from_compilation_db(&path)?;
+            let final_entries = entries_from_db.chain(entries);
+            self.writer.write(final_entries)
+        } else {
+            self.writer.write(entries)
+        }
+    }
+}
+
+impl AppendClangOutputWriter {
+    /// Reads the compilation database from a file.
+    ///
+    /// NOTE: The function is intentionally not getting any `&self` reference,
+    /// because the logic is not bound to the instance.
+    fn read_from_compilation_db(
+        source: &path::Path,
+    ) -> anyhow::Result<impl Iterator<Item = clang::Entry>> {
+        let source_copy = source.to_path_buf();
+
+        let file = fs::File::open(source)
+            .map(io::BufReader::new)
+            .with_context(|| format!("Failed to open file: {:?}", source))?;
+
+        let entries = clang::read(file).filter_map(move |candidate| match candidate {
+            Ok(entry) => Some(entry),
+            Err(error) => {
+                log::error!("Failed to read file: {:?}, reason: {}", source_copy, error);
+                None
+            }
+        });
+        Ok(entries)
+    }
+}
+
+/// Responsible for writing a JSON compilation database file atomically.
+///
+/// The file is first written to a temporary file and then renamed to the final file name.
+/// This ensures that the output file is not left in an inconsistent state in case of errors.
+struct AtomicClangOutputWriter {
+    writer: ClangOutputWriter,
+    temp_file_name: path::PathBuf,
+    final_file_name: path::PathBuf,
+}
+
+impl TryFrom<(&path::Path, &config::DuplicateFilter)> for AtomicClangOutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(value: (&path::Path, &config::DuplicateFilter)) -> Result<Self, Self::Error> {
+        let (file_name, config) = value;
+
+        let temp_file_name = file_name.with_extension("tmp");
+        let writer = ClangOutputWriter::try_from((temp_file_name.as_path(), config))?;
+
+        Ok(Self {
+            writer,
+            temp_file_name: temp_file_name.to_path_buf(),
+            final_file_name: file_name.to_path_buf(),
+        })
+    }
+}
+
+impl IteratorWriter<clang::Entry> for AtomicClangOutputWriter {
+    fn write(self, entries: impl Iterator<Item = clang::Entry>) -> anyhow::Result<()> {
+        let temp_file_name = self.temp_file_name.clone();
+        let final_file_name = self.final_file_name.clone();
+
+        self.writer.write(entries)?;
+
+        fs::rename(&temp_file_name, &final_file_name).with_context(|| {
+            format!(
+                "Failed to rename file from '{:?}' to '{:?}'.",
+                temp_file_name, final_file_name
+            )
+        })?;
+
+        Ok(())
+    }
+}
+
+/// Responsible for writing a JSON compilation database file from the given entries.
+///
+/// # Features
+/// - Writes the entries to a file.
+/// - Filters duplicates based on the provided configuration.
+struct ClangOutputWriter {
+    output: io::BufWriter<fs::File>,
+    filter: filter_duplicates::DuplicateFilter,
+}
+
+impl TryFrom<(&path::Path, &config::DuplicateFilter)> for ClangOutputWriter {
+    type Error = anyhow::Error;
+
+    fn try_from(value: (&path::Path, &config::DuplicateFilter)) -> Result<Self, Self::Error> {
+        let (file_name, config) = value;
+
+        let output = fs::File::create(file_name)
+            .map(io::BufWriter::new)
+            .with_context(|| format!("Failed to open file: {:?}", file_name))?;
+
+        let filter = filter_duplicates::DuplicateFilter::try_from(config.clone())?;
+
+        Ok(Self { output, filter })
+    }
+}
+
+impl IteratorWriter<clang::Entry> for ClangOutputWriter {
+    fn write(self, entries: impl Iterator<Item = clang::Entry>) -> anyhow::Result<()> {
+        let mut filter = self.filter.clone();
+        let filtered_entries = entries.filter(move |entry| filter.unique(entry));
+        clang::write(self.output, filtered_entries)?;
+        Ok(())
+    }
 }
