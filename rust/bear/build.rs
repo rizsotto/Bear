d75455d6ce55eeb1de1d45abb9020b46cb80a8fe
Laszlo Nagy <rizsotto@gmail.com>
2025-05-10 12:12:06 +1000

    remove rust implementation from the main branch

--- rust/bear/build.rs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-fn main() {
-    println!("cargo:rustc-env=WRAPPER_EXECUTABLE_PATH=/usr/libexec/bear/wrapper");
-    println!("cargo:rustc-env=PRELOAD_LIBRARY_PATH=/usr/libexec/bear/$LIB/libexec.so");
-}
--- rust/bear/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-pub mod args;
-pub mod config;
-mod fixtures;
-pub mod intercept;
-pub mod modes;
-pub mod output;
-pub mod semantic;
