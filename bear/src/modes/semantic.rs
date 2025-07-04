199ca1958ed42c6905bcc64feeffeaa3c099f6ab
Laszlo Nagy <rizsotto@gmail.com>
2025-07-04 19:19:12 +1000

    simplify semantic module

--- bear/src/modes/semantic.rs
+++ bear/src/modes/semantic.rs
@@ -4,41 +4,9 @@ use crate::intercept::Event;
 use crate::semantic::interpreters;
 use crate::{args, config, output, semantic};
 
-pub(super) struct SemanticAnalysis {
-    interpreter: Box<dyn semantic::Interpreter>,
-}
-
-impl TryFrom<&config::Main> for SemanticAnalysis {
-    type Error = anyhow::Error;
-
-    fn try_from(config: &config::Main) -> Result<Self, Self::Error> {
-        let interpreter = interpreters::create(config);
-
-        Ok(Self {
-            interpreter: Box::new(interpreter),
-        })
-    }
-}
-
-impl SemanticAnalysis {
-    pub fn analyze(&self, event: Event) -> Option<semantic::Command> {
-        log::debug!("event: {event}");
-        match self.interpreter.recognize(&event.execution) {
-            Some(recognized) => {
-                log::debug!("recognized semantic: {recognized:?}");
-                Some(recognized)
-            }
-            None => {
-                log::debug!("not recognized");
-                None
-            }
-        }
-    }
-}
-
 /// The semantic analysis that is independent of the event source.
 pub(super) struct SemanticAnalysisPipeline {
-    analyzer: SemanticAnalysis,
+    interpreter: Box<dyn semantic::Interpreter>,
     writer: output::OutputWriter,
 }
 
@@ -48,10 +16,13 @@ impl SemanticAnalysisPipeline {
         output: args::BuildSemantic,
         config: &config::Main,
     ) -> anyhow::Result<Self> {
-        let analyzer = SemanticAnalysis::try_from(config)?;
+        let interpreter = interpreters::create(config);
         let writer = output::OutputWriter::try_from((&output, &config.output))?;
 
-        Ok(Self { analyzer, writer })
+        Ok(Self {
+            interpreter: Box::new(interpreter),
+            writer,
+        })
     }
 
     /// Consumer the envelopes for analysis and write the result to the output file.
@@ -61,9 +32,10 @@ impl SemanticAnalysisPipeline {
         events: impl IntoIterator<Item = Event>,
     ) -> anyhow::Result<()> {
         // Set up the pipeline of compilation database entries.
-        let semantics = events
-            .into_iter()
-            .flat_map(|event| self.analyzer.analyze(event));
+        let semantics = events.into_iter().flat_map(|event| {
+            // FIXME: add logging for this step
+            self.interpreter.recognize(&event.execution)
+        });
         // Consume the entries and write them to the output file.
         // The exit code is based on the result of the output writer.
         self.writer.write(semantics)?;
